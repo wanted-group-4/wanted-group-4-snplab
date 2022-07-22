5eea5af3c35bf2fd3c7ae0ea8289d58a6d0a2660
mirim <alfla6528@naver.com>
2022-07-22 11:47:38 +0900

    chore: router setting & style provider setting

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,18 +1,16 @@
-import React from "react";
-import { Route, Routes } from "react-router";
+import React from 'react';
+import { Route, Routes } from 'react-router';
 
-import Admin from "pages/Admin";
-import Home from "pages/Home";
-import User from "pages/User";
+import Admin from 'pages/Admin';
+import Home from 'pages/Home';
+import User from 'pages/User';
 
 function Router() {
   return (
     <Routes>
       <Route path="" element={<Home />} />
       <Route path="user" element={<User />} />
-      <Route path="admin" element={<Admin />}>
-        <Route path="/:id" />
-      </Route>
+      <Route path="admin" element={<Admin />} />
     </Routes>
   );
 }
