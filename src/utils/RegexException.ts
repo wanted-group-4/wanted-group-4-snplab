944afb897781661cf7f6437ec0be0c2234f9e39f
greenish0902 <greenish0902@gmail.com>
2022-07-22 18:20:43 +0900

    feat: 유효성 검사 에러처리 로직 구현

---
+++ src/utils/RegexException.ts
@@ -0,0 +1,24 @@
+import { IField } from '../types/models/Fields';
+
+class RegexException extends Error {
+  _statusCode: number;
+  _field: IField | null;
+
+  constructor(msg = '잘못된 요청입니다', field: IField | null = null) {
+    super(msg);
+    this._statusCode = 400;
+    this._field = field;
+  }
+
+  get statusCode() {
+    return this._statusCode;
+  }
+  get field() {
+    return this._field;
+  }
+  set field(params) {
+    this._field = params;
+  }
+}
+
+export default RegexException;
