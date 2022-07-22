e63427e006834478416839662c1ca70d5712ca57
park_so_young <soyoung931014@gmail.com>
2022-07-22 17:42:53 +0900

    fix: region type 수정

--- src/types/models/region.ts
+++ src/types/models/region.ts
@@ -1,17 +1,3 @@
 export interface region {
-  서울특별시: string[];
-  인천광역시: string[];
-  부산광역시: string[];
-  대전광역시: string[];
-  울산광역시: string[];
-  광주광역시: string[];
-  제주특별자치도: string[];
-  세종특별자치시: string[];
-  경기도: string[];
-  강원도: string[];
-  충청남도: string[];
-  경상북도: string[];
-  경상남도: string[];
-  전라북도: string[];
-  전라남도: string[];
+  [key: string]: string[];
 }
