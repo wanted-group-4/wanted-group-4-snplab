b535cf600fc7ea8b51336c96d7a4ac2647f416cc
devmagrfs <devmagrfs@gmail.com>
2022-07-26 00:36:41 +0900

    fix: 사용하지 않는 코드로 인한 에러 수정

--- src/components/applicant/ApplicantContainer.tsx
+++ src/components/applicant/ApplicantContainer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 
 import ApplicantList from '@components/applicant/ApplicantList';
 
@@ -8,20 +7,6 @@ function ApplicantConainer() {
   const [userList, setUserList] = React.useState([]);
   const [keyList, setKeyList] = React.useState<Array<string>>([]);
   const [filterUserList, setFilterUserList] = React.useState([]);
-
-  React.useEffect(() => {
-    async function getUserData() {
-      try {
-        const userData = await axios.get(`/db.json`);
-        setUserList(userData.data.users);
-        const keyData = Object.keys(userData.data.users[0]);
-        setKeyList(keyData);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getUserData();
-  }, []);
 
   const userListFilter = () => {
     return setFilterUserList(userList);
