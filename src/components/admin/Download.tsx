0830eb485c422a6be774a8b5ca2091bcb7a1aabb
mirim <alfla6528@naver.com>
2022-07-25 12:17:13 +0900

    feat: 엑셀 다운로드 구현

--- src/components/admin/Download.tsx
+++ src/components/admin/Download.tsx
@@ -1,17 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function Download() {
-  return <DownloadContainer>CSV 다운로드</DownloadContainer>;
+interface DownloadProps {
+  data: { [key: string]: string }[];
 }
 
-const DownloadContainer = styled.div`
+export default function Download({ data }: DownloadProps) {
+  const download = (csvData: string) => {
+    const blob = new Blob([`\ufeff${csvData}`], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'download.csv');
+    link.click();
+  };
+
+  const changeFormatCsvData = (data: { [key: string]: string }[]) => {
+    const row: string[] = [];
+    const header = Object.keys(data[0]);
+    row.push(header.join(','));
+
+    data.map(item => {
+      const value = Object.values(item).join(',');
+      row.push(value);
+    });
+
+    return row.join('\n');
+  };
+
+  const getData = () => {
+    const csvData = changeFormatCsvData(data);
+    download(csvData);
+  };
+
+  return <DownloadButton onClick={getData}>엑셀 다운로드</DownloadButton>;
+}
+
+const DownloadButton = styled.button`
   width: 180px;
   height: 50px;
   background: ${({ theme }) => theme.color.grey_04};
   border-radius: 15px;
   padding: 15px;
   text-align: center;
+  font-size: 16px;
   font-weight: 700;
   cursor: pointer;
 `;
