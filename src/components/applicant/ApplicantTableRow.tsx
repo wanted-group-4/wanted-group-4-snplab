11ebb06725eb410ad55838f07785c6e5c0f39740
devmagrfs <devmagrfs@gmail.com>
2022-07-26 19:10:51 +0900

    refactor: 당첨여부 업데이트 로직 개선

--- src/components/applicant/ApplicantTableRow.tsx
+++ src/components/applicant/ApplicantTableRow.tsx
@@ -4,20 +4,16 @@ import styled from 'styled-components';
 import { patchUserWin } from '@api/adminApi';
 
 function ApplicantTableRow({ user }: any) {
-  const tableRowRef = React.useRef<HTMLTableSectionElement>(null);
   const updateWin = patchUserWin();
 
   const handleWinStatus = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (tableRowRef.current) {
-      const id = parseInt(tableRowRef.current.innerText.slice(0, 1));
-      const win = e.target.checked;
-      updateWin.mutate({ id, win });
-    }
-    return;
+    const id = user.id;
+    const win = e.target.checked;
+    updateWin.mutate({ id, win });
   };
 
   return (
-    <ListTableBody ref={tableRowRef}>
+    <ListTableBody>
       <ListTableTR>
         <ListTableTD>{user.id}</ListTableTD>
         <ListTableTD>{user.date}</ListTableTD>
@@ -26,21 +22,14 @@ function ApplicantTableRow({ user }: any) {
         <ListTableTD>{user.birth}</ListTableTD>
         <ListTableTD>{user.phone}</ListTableTD>
         <ListTableTD>{user.email}</ListTableTD>
-        <ListTableTD
-          style={{
-            overflowX: 'auto',
-            whiteSpace: 'nowrap',
-          }}
-        >
-          {user.transportation.join(', ')}
-        </ListTableTD>
+        <ListTableTD>{user.transportation.join(', ')}</ListTableTD>
         <ListTableTD>{user.region}</ListTableTD>
         <ListTableTD>
-          {user.win ? (
-            <input type="checkbox" defaultChecked onChange={handleWinStatus} />
-          ) : (
-            <input type="checkbox" onChange={handleWinStatus} />
-          )}
+          <WinCheckbox
+            type="checkbox"
+            defaultChecked={user.win}
+            onChange={handleWinStatus}
+          />
         </ListTableTD>
       </ListTableTR>
     </ListTableBody>
@@ -52,4 +41,5 @@ const ListTableTR = styled.tr``;
 const ListTableTD = styled.td`
   padding: 2px;
 `;
+const WinCheckbox = styled.input``;
 export default ApplicantTableRow;
